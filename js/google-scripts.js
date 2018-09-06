edafc886414eef976ef1674371cd80424ebde6a9
brunofreeman <33364693+brunofreeman@users.noreply.github.com>
2018-09-06 16:24:12 -0400

    moved CSS and JS to seperate files

---
+++ js/google-scripts.js
@@ -0,0 +1,69 @@
+//client secret = dL5r3yMMjmeHxKNPHAjHbu9f
+var CLIENT_ID = '835795750372-1hej792ghu2omccu5061a2nsrtrjuhh2.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyBt5O0GLMXaSYKphWXikYa9CQ2HcdiKhjw';
+var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+var SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
+
+function handleClientLoad() {
+	gapi.load('client:auth2', initClient);
+}
+
+function initClient() {
+	gapi.client.init({
+		apiKey: API_KEY,
+		clientId: CLIENT_ID,
+		discoveryDocs: DISCOVERY_DOCS,
+		scope: SCOPES
+	}).then(function() {
+		pullSheetData();
+	});
+}
+
+function pullSheetData() {
+	pullLetterDay();
+	pullAnnouncements();
+}
+
+var letterDayCell = 'Calendar!A1'
+
+function pullLetterDay() { //refresh on new day
+	gapi.client.sheets.spreadsheets.values.get({
+		spreadsheetId: '1ovki_EYdu7FC2OZKbxBvnwiKHkePdcamvjXzuXIw4NA',
+		range: letterDayCell,
+	}).then(function(response) {
+		var data = response.result.values;
+		$('#letter-day-section').html('<span>' + data[0][0] + '</span>');
+		resizeElements();
+	});
+}
+
+var announcementDataRange = 'Approved Announcements!C:E'
+var announcementColumn = 0;
+var titleColumn = 1;
+var imageURLColumn = 2;
+
+function pullAnnouncements() { //refresh every so often
+	gapi.client.sheets.spreadsheets.values.get({
+		spreadsheetId: '1ovki_EYdu7FC2OZKbxBvnwiKHkePdcamvjXzuXIw4NA',
+		range: announcementDataRange,
+	}).then(function(response) {
+		var data = response.result.values;
+		for (var i = 1; i < data.length; i++) {
+			var element = '';
+			if (i == 1) {
+				element = '<div class="item announcement-item active">';
+			} else {
+				element = '<div class="item announcement-item">';
+			}
+			if (!data[i][imageURLColumn]) {
+				data[i][imageURLColumn] = 'images\\default-announcement-image.png';
+			}
+			element += '<div class="announcement-item-text"><div class="announcement-item-part announcement-item-title"><span>' + data[i][titleColumn] + '</span></div>';
+			element += '<div class="announcement-item-part announcement-item-body"><span>' + data[i][announcementColumn] + '</span></div></div>';
+			element += '<div class="announcement-item-part announcement-item-img-div"><img class="announcement-item-img" src="' + data[i][imageURLColumn] + '"></div>';
+			element += '</div>';
+			$('#annoucement-carousel-item-container').append(element);
+		}
+		resizeElements();
+	});
+}
---
+++ js/site-scripts.js
@@ -0,0 +1,142 @@
+function resizeElements() {
+	$('#month-section').textfill({
+		maxFontPixels: 200
+	});
+	$('#date-section').textfill({
+		maxFontPixels: 200
+	});
+	$('#time-section').textfill({
+		maxFontPixels: 200
+	});
+	$('#ampm-section').textfill({
+		maxFontPixels: 200
+	});
+	$('#letter-day-section').textfill({
+		maxFontPixels: 200
+	});
+	$('#period-section').textfill({
+		maxFontPixels: 200
+	});
+	$('#cycle-section').textfill({
+		maxFontPixels: 200
+	});
+	$('.announcement-item-title').textfill({
+		maxFontPixels: 200
+	});
+	$('.announcement-item-body').textfill({
+		maxFontPixels: 200
+	});
+}
+
+function updateClock() {
+	var now = new Date();
+	var hour = now.getHours();
+	var minute = now.getMinutes();
+	var ampm = 'AM';
+	var period = 'X';
+	switch(hour) {
+		case 7:
+			period = 'HR';
+			break;
+		case 8:
+			if (minute >= 38) {
+				period = 'P2';
+			} else {
+				period = 'P1';
+			}
+			break;
+		case 9:
+			if (minute >=  20) {
+				period = 'P3';
+			} else {
+				period = 'P2';
+			}
+		case 10:
+			if (minute >= 44) {
+				period = 'P5';
+			} else if (minute >= 2) {
+				period = 'P4';
+			} else {
+				period = 'P3';
+			}
+			break;
+		case 11:
+			if (minute >= 26) {
+				period = 'P6';
+			} else {
+				period = 'P5';
+			}
+			break;
+		case 12:
+			if (minute >= 50) {
+				period = 'P8';
+			} else if (minute >= 8) {
+				period = 'P7';
+			} else {
+				period = 'P6';
+			}
+			break;
+		case 13:
+			if (minute >= 32) {
+				period = 'P9';
+			} else {
+				period = 'P8';
+			}
+			break;
+		case 14:
+			if (minute >= 45) {
+				period = 'AS';
+			} else {
+				period = 'AP';
+			}
+			break;
+		default:
+			period = 'AS';
+	}
+	if (hour == 0) {
+		hour = 12;
+	} else if (hour == 12) {
+		ampm = 'PM'
+	} else if (hour > 12) {
+		hour -= 12;
+		ampm = 'PM';
+	}
+	if (minute < 10) {
+		minute = '0' + minute;
+	}
+	$('#time-section').html('<span>' + hour + ':' + minute + '</span>');
+	$('#ampm-section').html('<span>' + ampm + '</span>')
+	var months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+	$('#month-section').html('<span>' + months[now.getMonth()] + '</span>');
+	$('#date-section').html('<span>' + now.getDate() + '</span>');
+	$('#period-section').html('<span>' + period + '</span>');
+	resizeElements();
+	setTimeout(updateClock, 5000);
+}
+
+window.addEventListener('resize', resizeElements);
+updateClock();
+$('#announcements-card, #slideshow-card').carousel();
+$('#announcements-card').on('slid.bs.carousel', function() {
+    resizeElements();
+});
+
+var dir = 'images\\slideshow-images\\';
+
+$.ajax({
+    url : dir,
+    success: function(data) {
+        $(data).find("a").attr("href", function (i, file) {
+            if (file.match(/\.(jpe?g|png|gif)$/)) {
+            	var element = '';
+            	if (i == 0) {
+            		element += '<div class="item active">';
+            	} else {
+            		element += '<div class="item">';
+            	}
+            	element += '<div class="slideshow-div"><img class="slideshow-img" src="' + dir + file + '"></div></div>';
+                $("#media-carousel-item-container").append(element);
+            } 
+        });
+    }
+});
