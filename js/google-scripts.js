7eea3f8d3513588f1ae9177374dfcc2c866628a8
brunofreeman <33364693+brunofreeman@users.noreply.github.com>
2018-09-25 12:00:35 -0400

    working out bug where screens freeze

--- js/google-scripts.js
+++ js/google-scripts.js
@@ -63,6 +63,7 @@ function pullAnnouncements() {
 			element += '<div class="announcement-item-part announcement-item-img-div"><img class="announcement-item-img" src="' + data[i][imageURLColumn] + '"></div>';
 			element += '</div>';
 			$('#annoucement-carousel-item-container').append(element);
+			console.log(i);
 		}
 		resizeElements();
 	});
